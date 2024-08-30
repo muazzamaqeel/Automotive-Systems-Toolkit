0832d9907ce54b6deffbd099ee4b9944348cbcc0
Muazzam Bin Aqeel <muazzamaqeel@outlook.com>
2024-08-30 11:26:32 +0200

    Add Tool

---
+++ V2X-VersionCheck/VersionInfoWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace V2X_VersionCheck
+{
+    public partial class VersionInfoWindow : Window
+    {
+        public VersionInfoWindow(string line2, string line5)
+        {
+            InitializeComponent();
+            VersionTextBlock.Text = $"Line 2: {line2}\nLine 5: {line5}";
+        }
+    }
+}
